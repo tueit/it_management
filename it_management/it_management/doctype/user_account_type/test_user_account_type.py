451154a014e6222426bd8b810afb8e524ea3ae24
templis <9048398+templis@users.noreply.github.com>
2025-08-12 23:40:48 +0200

    test: add User Account Type creation test

--- it_management/it_management/doctype/user_account_type/test_user_account_type.py
+++ it_management/it_management/doctype/user_account_type/test_user_account_type.py
@@ -3,8 +3,22 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
-import unittest
+import frappe
+from frappe.tests.utils import FrappeTestCase
 
-class TestUserAccountType(unittest.TestCase):
-	pass
+
+class TestUserAccountType(FrappeTestCase):
+    def test_user_account_type_creation(self):
+        doc = frappe.get_doc(
+            {
+                "doctype": "User Account Type",
+                "title": "Test Account Type",
+            }
+        )
+        doc.insert()
+
+        self.assertTrue(
+            frappe.db.exists("User Account Type", {"title": "Test Account Type"})
+        )
+
+        doc.delete()
